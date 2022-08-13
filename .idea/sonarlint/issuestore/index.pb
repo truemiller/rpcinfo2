24ee2c098ffbcfe6813e8ff591dcb97a1026fba4
Josh <31908788+truemiller@users.noreply.github.com>
2022-08-13 01:18:20 +0100

    added rpcs

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+<
+src/index.js,f\9\f900edc44cb929c2267a16100a32851f6d5b477d
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+F
+src/json/networks.json,5\9\598b3fc13b93c1ef7dd157b9ddd11d18940b0f74
+B
+src/json/rpcs.json,1\f\1f352e71e301b0ceb89dc88d1f12c56509988e5f
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+:
+
+src/App.js,1\e\1e43fa63aecd2127be034b3fb9d4caa172275714
--- src/App.js
+++ src/App.js
@@ -1,171 +1,221 @@
-import {useEffect, useState} from "react";
 import "bootswatch/dist/cosmo/bootstrap.min.css";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import NETWORKS from "./json/networks.json";
 import RPCS from "./json/rpcs.json";
 
-export default function Home () {
-      let [search, setSearch] = useState("");
-      let handleSearch = (event) => {
-        setSearch(event.target.value)
-        console.log(search)
-      };
+export default function Home() {
+  const handleAddToMetamask = (chainId) => {
+    console.debug(`Adding chainId "${chainId}" to Metamask`);
+    let rpc = RPCS.find((rpc) => rpc.chainId === chainId);
+    if (!rpc) return;
 
-      const handleAddToMetamask = (chainId) => {
-        console.debug(`Adding chainId "${chainId}" to Metamask`);
-        let rpc = RPCS.find(rpc => rpc.chainId === chainId);
-        if (!rpc) return;
-
-        let params = {
-          chainId: ('0x' + chainId.toString(16)),
-          blockExplorerUrls: [`${rpc.block_explorer}`],
-          chainName: rpc.name,
-          nativeCurrency: {
-            name: rpc.symbol,
-            symbol: rpc.symbol,
-            decimals: 18
-          },
-          rpcUrls: [`${rpc.url}`]
-        };
-        window.ethereum.enable().then(r => {
-          window.ethereum.request({method: 'eth_requestAccounts'}).then((p) => {
-            window.ethereum.request({
-              method: 'wallet_addEthereumChain',
-              params: [params, p],
-            })
-                .then((result) => {
-                  console.log(result)
-                })
-                .catch((error) => {
-                  console.log(error)
-                });
+    let params = {
+      chainId: "0x" + chainId.toString(16),
+      blockExplorerUrls: [`${rpc.block_explorer}`],
+      chainName: rpc.name,
+      nativeCurrency: {
+        name: rpc.symbol,
+        symbol: rpc.symbol,
+        decimals: 18,
+      },
+      rpcUrls: [`${rpc.url}`],
+    };
+    window.ethereum.enable().then((r) => {
+      window.ethereum.request({ method: "eth_requestAccounts" }).then((p) => {
+        window.ethereum
+          .request({
+            method: "wallet_addEthereumChain",
+            params: [params, p],
+          })
+          .then((result) => {
+            console.log(result);
+          })
+          .catch((error) => {
+            console.log(error);
           });
-        });
-
-      };
-
-      return (
-          <>
-            <Helmet>
-              <title>RPC Info</title>
-              <meta name="description"
-                    content="Metamask Custom RPCs, and public RPC API endpoints for blockchain networks. Including Avalanche, Binance Smart Chain, Matic and more."/>
-
-              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-203811220-1"/>
-              <script>
-                {`
+      });
+    });
+  };
+  return (
+    <>
+      <Helmet>
+        <title>RPC Info</title>
+        <meta
+          name="description"
+          content="Metamask Custom RPCs, and public RPC API endpoints for blockchain networks. Including Avalanche, Binance Smart Chain, Matic and more."
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-203811220-1"
+        />
+        <script>
+          {`
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', 'UA-203811220-1');
                 `}
-              </script>
-            </Helmet>
-            <div className={"flex-grow-1 h-100 bg-light"}>
-              <header className="navbar navbar-expand-lg bg-white">
-                <div className="container-fluid">
-                  <a href="#" className="navbar-brand">RPC Info</a>
-                  {NETWORKS.sort((a, b) => a.s > b.s).map((ntwk) => {
-                    return <a key={ntwk.s} className="nav-link text-decoration-none text-dark"
-                              href={`#${ntwk.s}-rpc`}>{ntwk.s.toUpperCase()}</a>
-                  })}
-                </div>
-              </header>
+        </script>
+      </Helmet>
+      <div className={" bg-light"}>
+        <nav className="navbar navbar-expand-lg bg-white">
+          <div className="container-fluid">
+            <a href="/" className="navbar-brand">
+              RPC Info
+            </a>
+          </div>
+        </nav>
 
-              <div className={"container"}>
-                <main className="mt-2">
-                  <article className="col-lg-12">
-                    <header className="flex-grow-1">
-                      <div className="d-flex flew-row justify-content-between">
-                        <h1 className="font-weight-bolder mb-0 display-1 fw-bolder">RPC Info</h1>
-                      </div>
-                      <p className="lead">Easily add <strong>custom RPCs</strong> to <a className="text-decoration-none"
-                                                                                        href="//metamask.io">Metamask</a>.</p>
-                      <div className="alert alert-success"><strong><a href="//twitter.com/joshmlxn">Follow me on
-                        Twitter</a></strong> for exclusive, new project announcements.
-                      </div>
-                      <input type="text" className="form-control form-control-lg mb-3 shadow-sm border"
-                             placeholder={"Search for a Network; ethereu.."} onInput={handleSearch}/>
-                    </header>
-                    <div className="row">
-                      {NETWORKS.sort((a, b) => {
-                        return a.s > b.s
-                      }).filter(network => {
-                        return network.s.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
-                            network.n.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-                      }).map(r => {
-                        return <section key={r.s} className={"col-md-12 card-body border mb-3"}>
-                          <h2 id={`${r.s}-rpc`} className={"fw-bold mb-2"}>
-                            <a className="text-decoration-none" href={r.u}>{r.n}</a> RPCs
-                          </h2>
-                          <div className={"row"}>
-                            <div className="col-md-12">
-                              <div className={"row"}>
-                                {RPCS.filter(x => x.network === r.s).map(p => {
-                                  return <div className={"col-md-4"} key={p.chainId}>
-                                    <div className="card mb-3">
-                                      <div className="card-body">
-                                        <h3 className={"border-bottom"}>{p.name}</h3>
-                                        <dl>
-                                          <dt>Type</dt>
-                                          <dd>{p.type}</dd>
-                                          <dt className={""}>RPC</dt>
-                                          <dd><a href={p.url}>{p.url}</a></dd>
-                                          <dt>Chain ID</dt>
-                                          <dd>{p.chainId}</dd>
-                                          <dt>Symbol</dt>
-                                          <dd>{p.symbol}</dd>
-                                          <dt>Block explorer</dt>
-                                          <dd><a href={p.block_explorer}>{p.block_explorer}</a></dd>
-                                          <>{p.chainId ? (<a onClick={() => handleAddToMetamask(p.chainId)}
-                                                             className={"btn btn-sm btn-primary rounded shadow mb-2"}>
-                                            Add to Metamask <img
-                                              src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"
-                                              height={13} width={13} alt="Metamask logo"
-                                              title={`Add ${p.name} RPC to Metamask`}>
-                                          </img></a>) : null}</>
-                                        </dl>
-                                      </div>
-                                    </div>
-                                  </div>
-                                })}
-                              </div>
-                            </div>
-                          </div>
-                        </section>
-                            ;
-                      })}
-                    </div>
-                    <section>
-                      <h2 id="faqs" className="">FAQs</h2>
-                      <dl>
-                        <dt>What does RPC stand for?</dt>
-                        <dd>RPC standards for Remote Procedural Call.</dd>
-                        <dt>What is a Metamask Custom RPC?</dt>
-                        <dd>A Metamask Custom RPC is an RPC that is added to the Metamask wallet so users
-                          can use other
-                          blockchains. For example, Binance Smart Chain, or Avalanche C-Chain.
-                        </dd>
-                        <dt>How can I add an RPC to the list?</dt>
-                        <dd><a href="//t.me/truemiller1">Contact True Miller via Telegram</a>.</dd>
-                      </dl>
-                    </section>
-                  </article>
-                </main>
+        <main className="container">
+          <header></header>
+          <section className="mt-2">
+            <header className="row">
+              <div className="d-flex flew-row justify-content-between">
+                <h1 className="font-weight-bolder mb-0 display-1 fw-bolder">
+                  RPC Info
+                </h1>
               </div>
-
-              <footer className="footer p-3 bg-white d-flex flex-row border-top">
-					<span className="text-muted align-self-start">&copy; <a href="https://mlxn.ltd"
-                                                                            className={"text-decoration-none"}>MLXN Ltd</a> 2020-2021. All rights
-						reserved.</span>
-                <span className={"mx-2"}><a href="//cryptologos.net"
-                                            className={"text-decoration-none"}>Crypto Logos</a></span>
-                <span className={"mx-2"}><a href="//twitter.com/joshmlxn" className={"text-decoration-none"}>Twitter</a></span>
-                <span className={"mx-2"}><a href="//t.me/truemiller1"
-                                            className={"text-decoration-none"}>Contact via Telegram</a></span>
-              </footer>
+              <p className="lead">
+                Easily add <strong>custom RPCs</strong> to{" "}
+                <a className="text-decoration-none" href="//metamask.io">
+                  Metamask
+                </a>
+                .
+              </p>
+              <div className="alert alert-success">
+                <strong>
+                  <a href="//twitter.com/joshmlxn">Follow me on Twitter</a>
+                </strong>{" "}
+                for exclusive, new project announcements.
+              </div>
+            </header>
+            <div className="row">
+              {NETWORKS.sort((a, b) => {
+                return a.s > b.s;
+              }).map((r) => {
+                return (
+                  <section
+                    key={r.s}
+                    className={"col-md-12 card-body border mb-3"}
+                  >
+                    <h2 id={`${r.s}-rpc`} className={"fw-bold mb-2"}>
+                      <a className="text-decoration-none" href={r.u}>
+                        {r.n}
+                      </a>{" "}
+                      RPCs
+                    </h2>
+                    <div className={"row"}>
+                      <div className="col-md-12">
+                        <div className={"row"}>
+                          {RPCS.filter((x) => x.network === r.s).map((p) => {
+                            return (
+                              <div className={"col-md-4"} key={p.chainId}>
+                                <div className="card mb-3">
+                                  <div className="card-body">
+                                    <h3 className={"border-bottom"}>
+                                      {p.name}
+                                    </h3>
+                                    <dl>
+                                      <dt>Type</dt>
+                                      <dd>{p.type}</dd>
+                                      <dt className={""}>RPC</dt>
+                                      <dd>
+                                        <a href={p.url}>{p.url}</a>
+                                      </dd>
+                                      <dt>Chain ID</dt>
+                                      <dd>{p.chainId}</dd>
+                                      <dt>Symbol</dt>
+                                      <dd>{p.symbol}</dd>
+                                      <dt>Block explorer</dt>
+                                      <dd>
+                                        <a href={p.block_explorer}>
+                                          {p.block_explorer}
+                                        </a>
+                                      </dd>
+                                      <>
+                                        {p.chainId ? (
+                                          <a
+                                            onClick={() =>
+                                              handleAddToMetamask(p.chainId)
+                                            }
+                                            className={
+                                              "btn btn-sm btn-primary rounded shadow mb-2"
+                                            }
+                                          >
+                                            Add to Metamask{" "}
+                                            <img
+                                              src="https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"
+                                              height={13}
+                                              width={13}
+                                              alt="Metamask logo"
+                                              title={`Add ${p.name} RPC to Metamask`}
+                                            ></img>
+                                          </a>
+                                        ) : null}
+                                      </>
+                                    </dl>
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                );
+              })}
             </div>
+            <section>
+              <h2 id="faqs" className="">
+                FAQs
+              </h2>
+              <dl>
+                <dt>What does RPC stand for?</dt>
+                <dd>RPC standards for Remote Procedural Call.</dd>
+                <dt>What is a Metamask Custom RPC?</dt>
+                <dd>
+                  A Metamask Custom RPC is an RPC that is added to the Metamask
+                  wallet so users can use other blockchains. For example,
+                  Binance Smart Chain, or Avalanche C-Chain.
+                </dd>
+                <dt>How can I add an RPC to the list?</dt>
+                <dd>
+                  <a href="//t.me/truemiller1">
+                    Contact True Miller via Telegram
+                  </a>
+                  .
+                </dd>
+              </dl>
+            </section>
+          </section>
+        </main>
 
-          </>)
-          ;
-    }
+        <footer className="footer p-3 bg-white d-flex flex-row border-top">
+          <span className="text-muted align-self-start">
+            &copy;{" "}
+            <a href="https://mlxn.ltd" className={"text-decoration-none"}>
+              MLXN Ltd
+            </a>{" "}
+            2020-2021. All rights reserved.
+          </span>
+          <span className={"mx-2"}>
+            <a href="//cryptologos.net" className={"text-decoration-none"}>
+              Crypto Logos
+            </a>
+          </span>
+          <span className={"mx-2"}>
+            <a href="//twitter.com/joshmlxn" className={"text-decoration-none"}>
+              Twitter
+            </a>
+          </span>
+          <span className={"mx-2"}>
+            <a href="//t.me/truemiller1" className={"text-decoration-none"}>
+              Contact via Telegram
+            </a>
+          </span>
+        </footer>
+      </div>
+    </>
+  );
+}
